ec3c14c3277ad0af8a451ab350618d7f89aae3c0
imardiego <imardiego@gmail.com>
2025-10-31 19:42:43 +0100

    s

---
+++ bell_cli/target/debug/build/quote-f77d8fbad66bb013/build_script_build-f77d8fbad66bb013.d
@@ -0,0 +1,5 @@
+/home/miguel/pruebasRust/bell_cli/target/debug/build/quote-f77d8fbad66bb013/build_script_build-f77d8fbad66bb013.d: /home/miguel/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.41/build.rs
+
+/home/miguel/pruebasRust/bell_cli/target/debug/build/quote-f77d8fbad66bb013/build_script_build-f77d8fbad66bb013: /home/miguel/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.41/build.rs
+
+/home/miguel/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.41/build.rs:
